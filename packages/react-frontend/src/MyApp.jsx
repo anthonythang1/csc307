d49f0d1ebc7140c00272eb1366ba1f164f5cf66b
anthonythang1 <anthonythang64@Yahoo.com>
2024-10-08 20:32:51 -0700

    fin

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -7,14 +7,25 @@ function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-            return i !== index;
-        });
-        setCharacters(updated);
-    }
+        const userToDelete = characters[index];
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
+        fetch(`http://localhost:8000/users/${userToDelete.id}`, {
+            method: 'DELETE',
+        })
+            .then((response) => {
+                if (response.status === 204) {
+                    // Successful delete, now update the frontend state
+                    const updated = characters.filter((character, i) => i !== index);
+                    setCharacters(updated);
+                } else if (response.status === 404) {
+                    console.log('User not found.');
+                } else {
+                    console.log('Failed to delete user. Status code:', response.status);
+                }
+            })
+            .catch((error) => {
+                console.log('Error while deleting user:', error);
+            });
     }
 
     function fetchUsers() {
@@ -23,30 +34,42 @@ function MyApp() {
     }
 
     function postUser(person) {
-        const promise = fetch("Http://localhost:8000/users", {
+        const promise = fetch("http://localhost:8000/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(person),
         });
-
         return promise;
     }
 
     function updateList(person) {
         postUser(person)
-            .then(() => setCharacters([...characters, person]))
+            .then((response) => {
+                if (response.status === 201) {
+                    return response.json();  
+                } else {
+                    console.log("User insertion failed with status:", response.status);
+                }
+            })
+            .then((updatedUser) => {
+                if (updatedUser) {
+                    setCharacters([...characters, updatedUser]);
+                }
+            })
             .catch((error) => {
                 console.log(error);
-            })
+            });
     }
 
     useEffect(() => {
         fetchUsers()
             .then((res) => res.json())
             .then((json) => setCharacters(json["users_list"]))
-            .catch((error) => { console.log(error); });
+            .catch((error) => {
+                console.log(error);
+            });
     }, []);
 
     return (
